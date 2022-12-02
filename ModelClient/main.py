1fc16e89c13de95281eebd156c1befa95c3fec1a
HubStudents <118428632+HubStudents@users.noreply.github.com>
2022-12-02 09:36:07 +0500

    Add files via upload

--- ModelClient/main.py
+++ ModelClient/main.py
@@ -12,31 +12,38 @@ generator = pipeline('text-generation', model = 'gpt2')
 
 def ask(textToCheck,qu):
     request = QA_pb2.QARequest(text=textToCheck, question=qu)
-    reply = str (stub.Ask(request))
+    reply = str (stub.Ask(request)).replace("answer:","")
     return reply
 
 def generateText(prompt,length,number):
     result = generator(prompt, max_length=length, num_return_sequences=number)
+    for i in range (len(result)):
+        result[i]= result[i]['generated_text']
     return result
 
 def printResult(list):
     for i in range (len(list)):
         print(list[i])
+        print("##########")
 
 
 if __name__ == '__main__':
 
-    textToCheck = """ World War I, also known as the Great War, began in 1914 after the assassination of Archduke Franz Ferdinand of Austria. 
-    His murder catapulted into a war across Europe that lasted until 1918. During the conflict, Germany, Austria-Hungary, Bulgaria and the Ottoman Empire (the Central Powers) 
-    fought against Great Britain, France, Russia, Italy, Romania, Canada, Japan and the United States (the Allied Powers). 
-    Thanks to new military technologies and the horrors of trench warfare, World War I saw unprecedented levels of carnage and destruction. 
-    By the time the war was over and the Allied Powers claimed victory, more than 16 million people—soldiers and civilians alike—were dead
+    textToCheck = """ 
+    An economic system is a means by which societies or governments organize and distribute available resources, services, and goods across a geographic region or country. 
+    Economic systems regulate the factors of production, including land, capital, labor, and physical resources. 
+    An economic system encompasses many institutions, agencies, entities, decision-making processes, and patterns of consumption that comprise the economic structure of a given community.
+    Capitalism generally features the private ownership of the means of production (capital) and a market economy for coordination. 
+    Corporate capitalism refers to a capitalist marketplace characterized by the dominance of hierarchical, bureaucratic corporations.
+    Mercantilism was the dominant model in Western Europe from the 16th to 18th century. 
+    This encouraged imperialism and colonialism until economic and political changes resulted in global decolonization. 
+    Modern capitalism has favored free trade to take advantage of increased efficiency due to national comparative advantage and economies of scale in a larger, more universal market. 
                 """
-    qu = "Which event caused world war 1 ?"
+    qu = "What are capitalism  characteristics ?"
 
-    print(ask(textToCheck, qu))
+    print( ask(textToCheck, qu))
 
-    printResult(generateText(ask(textToCheck, qu),100,5))
+    printResult ( generateText(ask(textToCheck, qu),150,10)  )
 
 
 
